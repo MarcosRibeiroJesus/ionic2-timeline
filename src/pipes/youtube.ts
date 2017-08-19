3e6549a9daa1bc78c95e3217e2fe8ae6f87c8da9
Marcos Ribeiro <marcoz.iu@gmail.com>
2017-08-19 18:32:27 -0300

    Timeline estática.
    Primeira versão da timeline.

---
+++ src/pipes/youtube.ts
@@ -0,0 +1,24 @@
+import { Pipe, PipeTransform, Injectable } from '@angular/core';
+import { DomSanitizer } from "@angular/platform-browser";
+/**
+ * Generated class for the Youtube pipe.
+ *
+ * See https://angular.io/docs/ts/latest/guide/pipes.html for more info on
+ * Angular Pipes.
+ */
+@Pipe({
+  name: 'youtube',
+})
+
+@Injectable()
+export class Youtube {
+  
+  constructor(private dom: DomSanitizer) {
+
+  }
+
+  transform(value, args) {
+    //console.log(value);
+    return this.dom.bypassSecurityTrustResourceUrl(value);
+  }
+}
